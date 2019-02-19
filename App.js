e1bf1290393599677e67dd579670e582e0191a57
Ioana Postolache <postolac@adobe.com>
2019-02-19 14:32:08 +0200

    -started-styling

---
+++ App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  TouchableWithoutFeedback
+} from "react-native";
+import AddEntry from "./components/AddEntry";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View style={{ flex: 1 }}>
+          <AddEntry alreadyLogged={false} />
+        </View>
+      </Provider>
+    );
+  }
+}
