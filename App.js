63289767888938bb3bef718096685f03d4c1303c
Ioana Postolache <postolac@adobe.com>
2019-02-28 19:03:07 +0200

    added-history-view

--- App.js
+++ App.js
@@ -1,24 +1,19 @@
 import React from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  TouchableHighlight,
-  TouchableNativeFeedback,
-  TouchableOpacity,
-  TouchableWithoutFeedback
-} from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import AddEntry from "./components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import History from "./components/History";
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <AddEntry alreadyLogged={false} />
+          {/*<AddEntry alreadyLogged={false} />*/}
+          <View style={{height:20}}/>
+          <History />
         </View>
       </Provider>
     );
--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,5 +1,11 @@
 import React, { Component } from "react";
-import { View, TouchableOpacity, Text } from "react-native";
+import {
+  View,
+  TouchableOpacity,
+  Text,
+  Platform,
+  StyleSheet
+} from "react-native";
 import {
   getMetricMetaInfo,
   timeToString,
@@ -13,11 +19,17 @@ import TextButton from "./TextButton";
 import { submitEntry, removeEntry } from "../utils/api";
 import { connect } from "react-redux";
 import { addEntry } from "../actions";
+import { white, purple } from "../utils/colors";
 
 function SubmitBtn({ onPress }) {
   return (
-    <TouchableOpacity onPress={onPress}>
-      <Text>SUBMIT</Text>
+    <TouchableOpacity
+      style={
+        Platform.OS === "ios" ? styles.iosSubmitBtn : styles.androidSubmitBtn
+      }
+      onPress={onPress}
+    >
+      <Text style={styles.submitBtnText}>SUBMIT</Text>
     </TouchableOpacity>
   );
 }
@@ -99,26 +111,31 @@ class AddEntry extends Component {
 
   render() {
     const metaInfo = getMetricMetaInfo();
-    console.log(this.props.alreadyLogged)
+    console.log(this.props.alreadyLogged);
     if (this.props.alreadyLogged) {
       return (
-        <View>
-          <Ionicons name={"md-happy"} size={100} />
+        <View style={styles.center}>
+          <Ionicons
+            name={Platform.OS === "ios" ? "ios-happy-outline" : "md-happy"}
+            size={100}
+          />
           <Text>You already logged your information for today</Text>
-          <TextButton onPress={this.reset}>Reset</TextButton>
+          <TextButton style={{ padding: 10 }} onPress={this.reset}>
+            Reset
+          </TextButton>
         </View>
       );
     }
 
     return (
-      <View>
+      <View style={styles.container}>
         <DateHeader date={new Date().toLocaleDateString()} />
         {Object.keys(metaInfo).map(key => {
           const { getIcon, type, ...rest } = metaInfo[key];
           const value = this.state[key];
 
           return (
-            <View key={key}>
+            <View key={key} style={styles.row}>
               {getIcon()}
               {type === "slider" ? (
                 <UdaciSlider
@@ -150,5 +167,49 @@ function mapStateToProps(state) {
     alreadyLogged: state[key] && typeof state[key].today === "undefined"
   };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: white
+  },
+  row: {
+    flexDirection: "row",
+    flex: 1,
+    alignItems: "center"
+  },
+  iosSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    borderRadius: 7,
+    height: 45,
+    marginLeft: 40,
+    marginRight: 40
+  },
+  androidSubmitBtn: {
+    backgroundColor: purple,
+    padding: 10,
+    paddingLeft: 30,
+    paddingRight: 30,
+    height: 45,
+    borderRadius: 2,
+    alignSelf: "flex-end",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  submitBtnText: {
+    color: white,
+    fontSize: 22,
+    textAlign: "center"
+  },
+  center: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 30,
+    marginLeft: 30
+  }
+});
 
 export default connect(mapStateToProps)(AddEntry);
