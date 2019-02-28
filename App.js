dd23e182a829e7c12d4d64ea9c6c6c31fe057560
Ioana Postolache <postolac@adobe.com>
2019-02-28 23:40:10 +0200

    started-navigation

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
 import React from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, Platform } from "react-native";
 import AddEntry from "./components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import History from "./components/History";
+import { TabNavigator } from "react-navigation";
+import { purple, white } from "./utils/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+
+const Tabs = TabNavigator(
+  {
+    History: {
+      screen: History,
+      navigationOptions: {
+        tabBarLabel: "History",
+        tabBarIcon: ({ tintColor }) => (
+          <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+        )
+      }
+    },
+    AddEntry: {
+      screen: AddEntry,
+      navigationOptions: {
+        tabBarLabel: "Add Entry",
+        tabBarIcons: ({ tintColor }) => (
+          <FontAwesome name="plus-square" size={30} color={tintColor} />
+        )
+      }
+    }
+  },
+  {
+    navigationOptions: {
+      header: null
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Platform.OS === "ios" ? purple : white,
+      style: {
+        height: 56,
+        backgroundColor: Platform.OS === "ios" ? white : purple,
+        shadowColor: "rgba(0, 0, 0, 0.24)",
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
+    }
+  }
+);
 
 export default class App extends React.Component {
   render() {
@@ -12,8 +59,8 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           {/*<AddEntry alreadyLogged={false} />*/}
-          <View style={{height:20}}/>
-          <History />
+          <View style={{ height: 20 }} />
+          <Tabs />
         </View>
       </Provider>
     );
