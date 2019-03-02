ea5bdf8f7964aa52f4f208a2ab1866b5cd64ee53
Ioana Postolache <postolac@adobe.com>
2019-03-02 21:08:51 +0200

    added-tab-navigation

--- App.js
+++ App.js
@@ -1,15 +1,24 @@
 import React from "react";
-import { View, Platform } from "react-native";
+import { View, Platform, StatusBar } from "react-native";
 import AddEntry from "./components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
 import History from "./components/History";
-import { TabNavigator } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation";
 import { purple, white } from "./utils/colors";
 import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import {Constants} from 'expo';
 
-const Tabs = TabNavigator(
+function UdaciStatusBar({backgroundColor, ...props}){
+  return(
+    <View style={{backgroundColor, height:Constants.statusBarHeight}}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
+
+const Tabs = createBottomTabNavigator(
   {
     History: {
       screen: History,
@@ -59,7 +68,8 @@ export default class App extends React.Component {
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
           {/*<AddEntry alreadyLogged={false} />*/}
-          <View style={{ height: 20 }} />
+          {/*<View style={{ height: 20 }} />*/}
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
           <Tabs />
         </View>
       </Provider>
